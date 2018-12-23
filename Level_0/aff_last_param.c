1cece03cc37a8fcdc255b987f6812df3b42e7a1c
Rudakevych <yurij.rudakevych@gmail.com>
2018-12-23 23:22:10 +0200

    added some tasks from Level_0 and Level_1

---
+++ Level_0/aff_last_param.c
@@ -0,0 +1,48 @@
+/*
+ * Task:
+
+Assignment name  : aff_last_param
+Expected files   : aff_last_param.c
+Allowed functions: write
+--------------------------------------------------------------------------------
+
+Write a program that takes strings as arguments, and displays its last
+argument followed by a newline.
+
+If the number of arguments is less than 1, the program displays a newline.
+
+Examples:
+
+$> ./aff_last_param "zaz" "mange" "des" "chats" | cat -e
+chats$
+$> ./aff_last_param "j'aime le savon" | cat -e
+j'aime le savon$
+$> ./aff_last_param
+$
+ */
+
+#include <unistd.h>
+
+void	ft_putstr(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+}
+
+int 	main(int argc, char **argv)
+{
+	if (argc < 2)
+		write(1, "\n", 1);
+	else
+	{
+		ft_putstr(argv[argc - 1]);
+		write(1, "\n", 1);
+	}
+	return (0);
+}
---
+++ Level_1/ft_strcpy.c
@@ -0,0 +1,47 @@
+/*
+ *
+ * Task:
+
+Assignment name  : ft_strcpy
+Expected files   : ft_strcpy.c
+Allowed functions:
+--------------------------------------------------------------------------------
+
+Reproduce the behavior of the function strcpy (man strcpy).
+
+Your function must be declared as follows:
+
+char    *ft_strcpy(char *s1, char *s2);
+
+*/
+
+#include <unistd.h>
+
+void	ft_putstr(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+}
+
+char    *ft_strcpy(char *s1, char *s2)
+{
+
+}
+
+int 	main()
+{
+	char *s1;
+	char *s2;
+
+	s1 = "Ho-ho-ho!";
+	s2 = "Hi all!";
+	ft_strcpy(s1, s2);
+	ft_putstr(s1);
+	return (0);
+}
