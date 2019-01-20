4e9a8c39e4dc6fe1a70597bc63bae6b24b1ffaa0
Rudakevych <yurij.rudakevych@gmail.com>
2019-01-20 13:58:31 +0200

    added some tasks from Level_4

--- Level_4/rostring.c
+++ Level_4/rostring.c
@@ -35,36 +35,78 @@
 #include <unistd.h>
 #include <stdlib.h>
 
+char 	*write_first_word(char *str, int i)
+{
+	char 	*first_word;
+	int 	tmp;
+	int 	j;
+	int 	first_word_len;
+
+	j = 0;
+	tmp = i;
+	while (str[tmp] != ' ' && str[tmp] != '\t')
+		tmp++;
+	first_word_len = tmp - i;
+	first_word = (char *)malloc(sizeof(char) * first_word_len + 1);
+	while (first_word_len > 0)
+	{
+		first_word[j] = str[i];
+		first_word_len--;
+		j++;
+		i++;
+	}
+	first_word[j] = '\0';
+	return (first_word);
+}
+
 void	rostring(char *str)
 {
 	char 	*first_word;
 	int 	i;
-	int 	first_word_len;
 	int 	is_space;
+	int 	j;
+	int 	second_part_len;
 
 	i = 0;
-//	first_word_len =
-	first_word = (char *)malloc(sizeof(char) * first_word_len);
+	j = 0;
 	is_space = 0;
+	second_part_len = 0;
 	while (str[i] == ' ' || str[i] == '\t')
 		i++;
-
-	// print from second word
-	while (str[i])
+	first_word = write_first_word(str, i);
+	while (str[i] != ' ' && str[i] != '\t')
+		i++;
+	while (str[i] == ' ' || str[i] == '\t')
+		i++;
+	while (str[i])	// print from second word to the end with one space
 	{
 		if (str[i] == ' ' || str[i] == '\t')
 			is_space = 1;
-
+		if (str[i] != ' ' && str[i] != '\t')
+		{
+			if (is_space == 1)
+				write(1, " ", 1);
+			is_space = 0;
+			write(1, &str[i], 1);
+		}
 		i++;
+		second_part_len++;
 	}
+	if (second_part_len > 0)
+		write(1, " ", 1);
+	while (first_word[j]) // print first word
+	{
+		write(1, &first_word[j], 1);
+		j++;
+	}
+	free(first_word);
 }
 
 int 	main(int ac, char **av)
 {
 	if (ac == 2)
-	{
 		rostring(av[1]);
-	}
 	write(1, "\n", 1);
+//	system("leaks rostring"); // check memory leaks
 	return (0);
 }
